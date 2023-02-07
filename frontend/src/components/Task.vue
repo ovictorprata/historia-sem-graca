adb95125ddb8218d246393088256078fcdc847c1
victor.prata <victor.prata@buser.com.br>
2023-02-06 21:30:56 -0300

    first commit

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <div>#{{ task.id }}</div>
+      <p class="ma-0 pa-0 text-h5 text--primary">
+        {{ task.description }}
+      </p>
+      <v-btn icon="mdi-heart" :to="{ name: 'base-home' }"></v-btn>
+      <v-btn icon="mdi-trash-can" :to="{ name: 'base-home' }"></v-btn>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TasksModel",
+  props: {
+    task: {
+      type: Object,
+      default: null,
+      roger: "usalinter",
+    },
+  },
+  data: () => ({}),
+}
+</script>
